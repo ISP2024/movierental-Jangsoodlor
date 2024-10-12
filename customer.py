b5811b4cbf343f6a51ed78278ea5c1d45ba4be59
Rattapoom Pethputong <celica2548@gmail.com>
2024-10-12 14:31:49 +0700

    Extract Method to compute total charge

--- customer.py
+++ customer.py
@@ -1,5 +1,5 @@
 from rental import Rental
-from movie import Movie
+
 
 class Customer:
     """A customer who rents movies.
@@ -18,45 +18,52 @@ class Customer:
         """Add a rental for this customer"""
         if rental not in self.rentals:
             self.rentals.append(rental)
-    
+
     def get_name(self):
         """Get the customer's name."""
         return self.name
-    
+
     def statement(self):
         """Create a statement of rentals for the current period.
 
-        Print all the rentals in the current period, 
+        Print all the rentals in the current period,
         along with total charges and frequent renter points.
-        
+
         Returns:
             the statement as a String
         """
-        total_amount = 0   # total rental charges
         frequent_renter_points = 0
         # the .format method substitutes actual values into the fmt string
         statement = f"Rental Report for {self.name}\n\n"
         header_fmt = "{:40s}  {:6s} {:6s}\n"
         statement += header_fmt.format("Movie Title", "  Days", " Price")
         rental_fmt = "{:40s}  {:6d} {:6.2f}\n"
-        
+
         for rental in self.rentals:
             # compute rental change
             # compute the frequent renter points based on movie price code
             frequent_renter_points += rental.rental_points()
             #  add a detail line to statement
             statement += rental_fmt.format(
-                            rental.get_movie().get_title(), 
-                            rental.get_days_rented(), 
-                            rental.get_price())
+                rental.get_movie().get_title(),
+                rental.get_days_rented(),
+                rental.get_price(),
+            )
             # and accumulate activity
-            total_amount += rental.get_price()
 
         # footer: summary of charges
         statement += "\n"
         statement += "{:40s}  {:6s} {:6.2f}\n".format(
-                       "Total Charges", "", total_amount)
-        statement += "Frequent Renter Points earned: {}\n".format(frequent_renter_points)
+            "Total Charges", "", self.total_amount()
+        )
+        statement += "Frequent Renter Points earned: {}\n".format(
+            frequent_renter_points
+        )
 
         return statement
 
+    def total_amount(self):
+        total_amount = 0
+        for rental in self.rentals:
+            total_amount += rental.get_price()
+        return total_amount
