3008c1cfbb5171274e1232302a59cc8703f1190c
Rattapoom Pethputong <celica2548@gmail.com>
2024-10-19 20:39:58 +0700

    add attributes to movie and make it immutable

--- movie.py
+++ movie.py
@@ -1,14 +1,24 @@
+from collections.abc import Collection
+from dataclasses import dataclass
+
+
+@dataclass(frozen=True)
 class Movie:
     """
     A movie available for rent.
     """
 
-    def __init__(self, title):
+    def __init__(self, title: str, year: int, genre: Collection[str]):
         # Initialize a new movie.
         self.title = title
+        self.year = year
+        self.genre = genre
 
-    def get_title(self):
+    def get_title(self) -> str:
         return self.title
 
-    def __str__(self):
-        return self.title
+    def is_genre(self, genre: str):
+        return genre.lower() == self.genre.lower()
+
+    def __str__(self) -> str:
+        return f"{self.title} {self.year}"
