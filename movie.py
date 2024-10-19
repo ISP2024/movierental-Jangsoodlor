09e482c12c98c5811806d6c404ab3bfbce0c5e99
Rattapoom Pethputong <celica2548@gmail.com>
2024-10-19 19:31:17 +0700

    Remove middleman from Movie

--- movie.py
+++ movie.py
@@ -3,23 +3,16 @@ class Movie:
     A movie available for rent.
     """
 
-    def __init__(self, title, price_code):
+    def __init__(self, title):
         # Initialize a new movie.
         self.title = title
-        self.price_code = price_code
-
-    def get_price_code(self):
-        # get the price code
-        return self.price_code
 
     def get_title(self):
         return self.title
 
-    def get_rental_points(self, days):
-        return self.price_code.get_rental_points(days)
-
-    def get_price(self, days):
-        return self.price_code.get_price(days)
+    def get_price_code(self):
+        # get the price code
+        return self.price_code
 
     def __str__(self):
         return self.title
